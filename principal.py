c1fac686634571c2d00e8251ce643451f8e740ca
jc-ios <julio.aguilar@ues.edu.es>
2020-09-06 22:40:09 -0600

    Primerejercicio

---
+++ principal.py
@@ -0,0 +1,13 @@
+# primer ejemplo de uso de for
+# es un palindrome
+linea = input("Digite una palabra")
+esPalindrome = True
+for i in range(0, len(linea)//2):
+    if linea[i] != linea[len(linea)-i-1]:
+        esPalindrome = False
+        print("falso")
+        break
+if esPalindrome:
+    print("es palindrome ")
+else:
+    print("no es palindrome")
